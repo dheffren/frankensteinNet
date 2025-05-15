mostly got things working, but need to fix the visualization - problem is that the Normalization methods only look at INPUTS rather than outputs - need to fix that by adding another class or something. Furthermore, need to check some of the class prepare input methods for dict inputs to see if can make more or less general.

data.py
@@ -1,9 +1,10 @@
 import torch
 from torchvision import datasets, transforms
 from torch.utils.data import DataLoader, random_split
-from normalize import Normalizer
+from normalize import Normalizer, build_normalizers
 from pathlib import Path
 import yaml
+from transformations import TransformRegistry, build_transforms
 
 """
 Note: Write a custom method for each potential "type" of dataset i have. 
@@ -11,60 +12,83 @@ Then call this from the utils with the data path specified.
 TODO: Get this method to work with loaded data, make sure it works period. See where this is called in setup. 
 """
 def get_dataloaders(config, train = True):
-    cfg = config["data"]
-    full_dataset, found, mean, std = get_dataset(config, train)
-    val_len = int(cfg["val_split"] * len(full_dataset))
+    cfgD = config["data"]
+    prepare_dataset(cfgD) #prepares the mean and std for normalization.  
+    path = cfgD["path"]
+    
+    stats = load_normalization_stats(path) # Dictionary with mean, std. 
+    
+    transforms = build_transforms(cfgD, stats) #transform Registry object. 
+    full_dataset = get_dataset(cfgD, train, transforms)
+    val_len = int(cfgD["val_split"] * len(full_dataset))
     train_len = len(full_dataset) - val_len
     train_set, val_set = random_split(full_dataset, [train_len, val_len])
 
-    train_loader = DataLoader(train_set, batch_size=cfg["batch_size"], shuffle=cfg["shuffle"], num_workers=cfg["num_workers"])
-    val_loader = DataLoader(val_set, batch_size=cfg["batch_size"], shuffle=False, num_workers=cfg["num_workers"])
-    if not found: 
-        mean, std = compute_mean_std(train_loader)
-        save_normalization_stats(mean, std,cfg["path"])
-        #now data isn't loaded with normalizations - need to redo. 
-        #NOTE: The "different thing" is that we saved a file outside of the program. 
-        return get_dataloaders(config, train = True)
+    train_loader = DataLoader(train_set, batch_size=cfgD["batch_size"], shuffle=cfgD["shuffle"], num_workers=cfgD["num_workers"])
+    val_loader = DataLoader(val_set, batch_size=cfgD["batch_size"], shuffle=False, num_workers=cfgD["num_workers"])
+
     metadata = full_dataset.get_metadata()
     #need to complete logic.
-    normalizer = Normalizer(mean, std, mode="zscore")
+    normalizer = build_normalizers(stats)
     metadata["normalizer"] = normalizer
+    #TODO: Make normalizer work with the rest of the project.  
     return {"train": train_loader, "val":val_loader}, metadata
+def prepare_dataset(cfgD): #config["data"]
+    path = cfgD["path"]
+    try: 
+        stats = load_normalization_stats(path) #won't return empty dictionary - may return empty dict values. 
+        validate_normalization_stats(stats, cfgD)
+    except (FileNotFoundError, ValueError, TypeError) as e:
+        print(f"[INFO] Invalid or missing normalization info: {e}")
+        raw_ds = get_dataset(cfgD, transform=None)
+        stat_loader = torch.utils.data.DataLoader(
+            raw_ds, 
+            batch_size = cfgD["batch_size"], 
+            shuffle = False, 
+            num_workers = cfgD["num_workers"])
+        #compute ONLY for image keys. 
+        #TODO: input right keys. It seems like it naturally checks that it only does the right ones? But are we sure? 
+        #TODO: I think this is BACKWARD. - the alt one is backwards. Check if other one works. 
+        keys = [k for k, tlist in cfgD["transforms"].items() if "Normalize" in tlist]
+        stats = compute_mean_std_for_keys(stat_loader, keys = keys)
+        print(stats)
+        #save mean and std to disk. 
+        save_normalization_stats(stats,  path)
+    #after this - know it works. Thus, can proceed normally
+    return 
+def validate_normalization_stats(stats:dict, cfgD:dict):
+    #TODO: Should logic for transform keys instead come from the data itself? Ie from where we convert the data via dictionary. 
+    transform_keys = cfgD["transforms"].keys() #list of ALL transform keys - not just those with normalization. 
+    for key in transform_keys:
+        if "Normalize" not in cfgD["transforms"][key]: continue # Check if normalize is in the list of transforms for that given key.  
+        if key not in stats["mean"] or key not in stats["std"]: #throw error, this shouldn't happen if file is correct. 
+            raise ValueError(f"Missing mean/std for key '{key}' in metadata.")
+        mean = stats["mean"][key]
+        std  = stats["std"][key]
+        if not isinstance(mean, list) or not isinstance(std, list):
+            raise TypeError(f"Mean/std for '{key}' should be lists, got {type(mean)} / {type(std)}")
+        if len(mean) != len(std):
+            raise ValueError(f"Mean/std for '{key}' must have same length: {mean} vs {std}")
+        #check value ranges. 
+        if not all(std>0): 
+            raise ValueError("Std at or below 0")
+        #check shape matches with dataset. 
 
-def get_dataset(config, train = True):
-    #TODO: Configure this normalization stuff to work in the general case: with multiple inputs or whatever. 
-    dataset_name = config["data"]["dataset"]
-    print(f"dataset_name: {dataset_name}")
-    path = config["data"]["path"]
-    found = True
-    mean = None
-    std = None
-    try:
-        mean, std = load_normalization_stats(path)
-        transform = get_transforms(config, train = train, apply_normalization=True, mean=mean, std=std)
-    except Exception as e:
-        transform = get_transforms(config, train = train)
-        found = False
+
+def get_dataset(cfgD, train = True, transform = None): 
+    dataset_name = cfgD["dataset"]
+    path = cfgD["path"]
     from datasets.data_registry import get_registered_dataset
     DatasetClass = get_registered_dataset(dataset_name)
     #should I specify a path to download FROM vs root? 
-    return DatasetClass(root = path, train = train, transform = transform), found, mean, std
+    #TODO: Check class takes in right transformation object. And that it performs transformation right. 
+    return DatasetClass(root = path, train = train, transform = transform)
 
-def get_transforms(config, train = True, apply_normalization = False, mean = None, std = None):
-    #THIS IS THE PROBLEM. 
-    #TODO: Adjust this to properly normalize the input images. 
-    tfm_cfg = config.get("transform_config", {})
-    tfms = [transforms.ToTensor()]
-    if apply_normalization: 
-        tfms.append(transforms.Normalize(mean, std))
-    #add additional augmentations here. 
-    if train:
-        pass
-    else: 
-        pass
-    return transforms.Compose(tfms)
 
 def load_normalization_stats(dataset_root):
+    """
+    csv data saved as a dict of mean and std, where each of those two can have different "input keys". 
+    """
     path = Path(dataset_root)/ "normalization.yaml"
     if not path.exists():
         raise FileNotFoundError(f"No normalization.yaml found in {dataset_root}")
@@ -72,13 +96,69 @@ def load_normalization_stats(dataset_root):
         stats = yaml.safe_load(f)
     if not ("mean" in stats and "std" in stats):
         raise ValueError(f"normalization.yaml exists but is missing keys: {stats}")
-    return stats["mean"], stats["std"]
+    #Not sure the default save is configured for this. Might have to create manually. 
+    if (not isinstance(stats["mean"], dict)) or (not isinstance(stats["std"], dict)):
+        raise ValueError(f"normalization.yaml exists but the keys aren't dictionaries. ")
+    return stats
 
-def save_normalization_stats(mean, std, path):
+def save_normalization_stats(stats, path):
     name = "normalization.yaml"
     pathL = Path(path) / name
     with open(pathL, "w") as f:
-        yaml.dump({"mean": mean.tolist(), "std": std.tolist()}, f)
+        #TODO: Fix this for new layout. .tolist is wrong. 
+        yaml.dump({"mean": stats["mean"], "std":stats["std"]}, f)
+def compute_mean_std_for_keys(loader, keys, eps = 1e-6):
+    """
+    Note: Need data to be returned/saved as a dictionary in this case. 
+    Keys = JUST keys that we're going to normalize with. 
+    """
+    stats = {k: {"sum": 0., "sum_sq": 0., "count": 0} for k in keys}
+    for batch in loader:
+        for k in keys:
+            if k not in batch: continue
+            x = batch[k]  # shape: [B, C, H, W]
+            if not is_image_like(x): continuel
+            B, C, H, W = x.shape
+            stats[k]["sum"]    += x.sum(dim=(0, 2, 3))  # sum over pixels per channel
+            stats[k]["sum_sq"] += (x ** 2).sum(dim=(0, 2, 3))
+            stats[k]["count"]  += B * H * W
+        means = {}
+    stds = {}
+    for k in keys:
+        s = stats[k]
+        mean = s["sum"] / s["count"]
+        std_raw = ((s["sum_sq"] / s["count"]) - mean**2).sqrt()
+        std = std_raw.clamp(min = eps)
+        means[k] = mean.tolist()
+        stds[k]  = std.tolist()
+    stats = {"mean": means, "std": stds}
+    print("stats: ", stats)
+    return stats
+def compute_mean_std_for_keysAlt(loader, keys):
+    #TODO: THIS IS THE WRONG WAY. INSIDE AND OUTSIDE KEYS REVERSED. 
+    #Figure out which of these we want. should do normalize_keys = [
+    #k for k, tlist in cfg["transforms"].items()
+    #if "Normalize" in tlist]
+    stats = {}
+    for k in keys:
+        sums, sqs, count = 0., 0., 0
+        for batch in loader:
+            if k not in batch: continue
+            x = batch[k]
+            if not is_image_like(x): continue
+            B, C, H, W = x.shape
+            sums += x.sum(dim=(0, 2, 3))
+            sqs  += (x ** 2).sum(dim=(0, 2, 3))
+            count += B * H * W
+        if count > 0:
+            mean = (sums / count).tolist()
+            std = ((sqs / count - (sums / count)**2).sqrt()).tolist()
+            stats[k] = {"mean": mean, "std": std}
+    return stats
+def is_image_like(t: torch.Tensor):
+
+    return t.ndim == 4 or (t.ndim == 3 and t.shape[0] in (1, 3))
+
 def compute_mean_std(loader):
     mean = 0.
     std = 0.

datasets/ballRotating.py
@@ -7,6 +7,7 @@ import os
 import csv
 import pandas as pd
 from pathlib import Path
+from torchvision import transforms
 @register_dataset("ballRotating")
 class BallRotatingDataset(torch.utils.data.Dataset):
     #TODO: I have a feeling the root should just be "data/" to be consistent with MNIST, then it should add its own name here. 
@@ -31,16 +32,25 @@ class BallRotatingDataset(torch.utils.data.Dataset):
         return self.dataAmt
 
     def __getitem__(self, idx):
+      
         #TODO: Add caching or lazy loading or something faster. 
         row = self.entries.iloc[idx]
         x1 = Image.open(row["path1"])
         x2 = Image.open(row["path2"])
-
+        #return data as a dict. 
+        element = {"x1": x1, "x2":x2}
+        #includes normalization. 
+       
         if self.transform:
-            x1 = self.transform(x1)
-            x2 = self.transform(x2)
-
-        return x1, x2
+      
+            transformedEle = self.transform(element)
+            return transformedEle
+        else: 
+            #still convert PIL image to tensor. 
+            transform = transforms.Compose([transforms.ToTensor()])
+             
+            element = {"x1": transform(x1), "x2":transform(x2)}
+            return element
     def get_metadata(self):
 
         return {

models/autoencoder.py
@@ -67,14 +67,17 @@ class Autoencoder(nn.Module):
                 targets = {"recon_target": {"x": x}, "labels":{"y": y}}  # recon + label
             return inputs, targets
         elif isinstance(batch, dict):
-            #TODO: Fix this to fit the above stuff. 
+            #TODO: Make this more general? 
+            #I dont' understand this code. 
             x = batch["x"]
-            inputs  = {k: (v.to(self.device) if torch.is_tensor(v) else v)
-                       for k, v in batch.items()
-                       if k in {"x"} or k.startswith("cond_")}  # whatever you forward
-            targets = {k: v for k, v in batch.items()
-                       if k not in inputs}
-            targets.setdefault("x", x)               # recon target by default
+            y = batch["y"]
+            inputs  = {"x": x.to(self.device).requires_grad_(requires_grad)}
+            #inputs  = {k: (v.to(self.device) if torch.is_tensor(v) else v)
+                    #   for k, v in batch.items()
+                      # if k in {"x"} or k.startswith("x")}  # whatever you forward
+            #ideally would make more generic, don't really have to here though. 
+            targets = {"recon_target": {"x": x}, "labels":{"y": y}}  # recon + label
+                      # recon target by default
             return inputs, targets
 
         raise TypeError("Unknown batch format for prepare_input")